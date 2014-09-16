fa0e19be806d67e53abffd3cc1d083707ae35897
Andy Grunwald <andygrunwald@gmail.com>
2014-09-16 08:51:02 +0200

    Fix #51: Add support for 4 byte UTF-8 character in MySQL

--- db/data_model_mysql.sql
+++ db/data_model_mysql.sql
@@ -2,16 +2,16 @@
 
 -- Create tables --
 CREATE TABLE mailing_lists (
-    mailing_list_url VARCHAR(255) CHARACTER SET utf8 NOT NULL,
-    mailing_list_name VARCHAR(255) CHARACTER SET utf8 NULL DEFAULT 'NULL',
-    project_name VARCHAR(255) CHARACTER SET utf8 NULL DEFAULT 'NULL',
+    mailing_list_url VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL,
+    mailing_list_name VARCHAR(255) CHARACTER SET utf8mb4 NULL DEFAULT 'NULL',
+    project_name VARCHAR(255) CHARACTER SET utf8mb4 NULL DEFAULT 'NULL',
     last_analysis DATETIME NULL,
     PRIMARY KEY(mailing_list_url))
 ENGINE=InnoDB;
 
 CREATE TABLE compressed_files (
-    url varchar(255) CHARACTER SET utf8 NOT NULL,
-    mailing_list_url varchar(255) CHARACTER SET utf8 not null,
+    url varchar(191) CHARACTER SET utf8mb4 NOT NULL,
+    mailing_list_url varchar(191) CHARACTER SET utf8mb4 not null,
     status enum('new','visited','failed') null,
     last_analysis datetime null,
     primary key(url),
@@ -20,26 +20,26 @@ CREATE TABLE compressed_files (
 ENGINE=INNODB;
 
 CREATE TABLE people (
-    email_address VARCHAR(255) CHARACTER SET utf8 NOT NULL,
-    name VARCHAR(255) CHARACTER SET utf8 NULL,
-    username VARCHAR(255) CHARACTER SET utf8 NULL,
-    domain_name VARCHAR(255) CHARACTER SET utf8 NULL,
-    top_level_domain VARCHAR(255) CHARACTER SET utf8 NULL,
+    email_address VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL,
+    name VARCHAR(255) CHARACTER SET utf8mb4 NULL,
+    username VARCHAR(255) CHARACTER SET utf8mb4 NULL,
+    domain_name VARCHAR(255) CHARACTER SET utf8mb4 NULL,
+    top_level_domain VARCHAR(255) CHARACTER SET utf8mb4 NULL,
     PRIMARY KEY(email_address))
 ENGINE=InnoDB;
 
 CREATE TABLE messages (
-    message_ID VARCHAR(255) CHARACTER SET utf8 NOT NULL,
-    mailing_list_url VARCHAR(255) CHARACTER SET utf8 NOT NULL,
-    mailing_list VARCHAR(255) CHARACTER SET utf8 NULL,
+    message_ID VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL,
+    mailing_list_url VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL,
+    mailing_list VARCHAR(255) CHARACTER SET utf8mb4 NULL,
     first_date DATETIME NULL,
     first_date_tz INTEGER(11) NULL,
     arrival_date DATETIME NULL,
     arrival_date_tz INTEGER(11) NULL,
-    subject VARCHAR(255) CHARACTER SET utf8 NULL,
-    message_body MEDIUMTEXT CHARACTER SET utf8 NULL,
-    is_response_of VARCHAR(255) CHARACTER SET utf8 NULL,
-    mail_path TEXT CHARACTER SET utf8 NULL,
+    subject VARCHAR(255) CHARACTER SET utf8mb4 NULL,
+    message_body MEDIUMTEXT CHARACTER SET utf8mb4 NULL,
+    is_response_of VARCHAR(191) CHARACTER SET utf8mb4 NULL,
+    mail_path TEXT CHARACTER SET utf8mb4 NULL,
     PRIMARY KEY(message_ID),
     INDEX response(is_response_of),
     FOREIGN KEY(mailing_list_url)
@@ -50,8 +50,8 @@ ENGINE=InnoDB;
 
 CREATE TABLE messages_people (
     type_of_recipient ENUM('From','To','Cc') NOT NULL DEFAULT 'From',
-    message_id VARCHAR(255) CHARACTER SET utf8 NOT NULL,
-    email_address VARCHAR(255) CHARACTER SET utf8 NOT NULL,
+    message_id VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL,
+    email_address VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL,
     PRIMARY KEY(type_of_recipient, message_id, email_address),
     INDEX m_id(message_id),
     FOREIGN KEY(message_ID) REFERENCES messages(message_id)
@@ -63,8 +63,8 @@ CREATE TABLE messages_people (
 ENGINE=InnoDB;
 
 CREATE TABLE mailing_lists_people (
-    email_address VARCHAR(255) CHARACTER SET utf8 NOT NULL,
-    mailing_list_url VARCHAR(255) CHARACTER SET utf8 NOT NULL,
+    email_address VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL,
+    mailing_list_url VARCHAR(191) CHARACTER SET utf8mb4 NOT NULL,
     PRIMARY KEY(email_address, mailing_list_url),
     FOREIGN KEY(mailing_list_url)
         REFERENCES mailing_lists(mailing_list_url)
