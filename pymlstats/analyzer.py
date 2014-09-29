11a5b04d4b8d8d3154fa9959d8c4afc04a225130
Andy Grunwald <andygrunwald@gmail.com>
2014-09-29 22:33:41 +0200

    Fix #51: Decode E-Mail-Address from header as well

--- pymlstats/analyzer.py
+++ pymlstats/analyzer.py
@@ -138,7 +138,7 @@ class MailArchiveAnalyzer:
                 address = self.__check_spam_obscuring([address])
                 addresses = []
                 for name, email in getaddresses(address):
-                    addresses.append((self.__decode(name, charset), email))
+                    addresses.append((self.__decode(name, charset), self.__decode(email, charset)))
 
                 filtered_message[header] = addresses
 
@@ -147,7 +147,7 @@ class MailArchiveAnalyzer:
             for address in (message.get_all('cc', [])):
                 address = self.__check_spam_obscuring([address])
                 for name, email in getaddresses(address):
-                    addresses.append((self.__decode(name, charset), email))
+                    addresses.append((self.__decode(name, charset), self.__decode(email, charset)))
 
             filtered_message['cc'] = addresses or None  # [('','')]
 
